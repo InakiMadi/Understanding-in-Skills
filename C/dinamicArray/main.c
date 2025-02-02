d02dcff06b6ae8d1cfd2aaf0f2a4df0c64e56c19
InakiMadi <inakimadi@gmail.com>
2025-02-02 03:54:31 +0100

    WIP debug not working, program is fine

--- C/dinamicArray/main.c
+++ C/dinamicArray/main.c
@@ -1,21 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "src/dinamicArray.h"
 
-// Declare the function sum.
-int sum(int a, int b);
 
-int main(int argc, char* argv[]){
-    if(argc != 3){
-        printf("Usage: %s <number1> <number2>.\n", argv[0]);
-        return 1; // Ends with error.
+
+
+
+//int main(int argc, char* argv[])
+int main()
+{
+    struct QueryCollection query_collection;
+    struct Query query;
+    initializeQueryCollection(&query_collection);
+
+    /*
+    * This stores the total number of books in each shelf.
+    
+    int* total_number_of_books;
+
+    *
+    * This stores the total number of pages in each book of each shelf.
+    * The rows represent the shelves and the columns represent the books.
+    
+    int** total_number_of_pages;
+    */
+
+    int total_number_of_shelves;
+    scanf("%d", &total_number_of_shelves);
+    
+    // Number of queries
+    
+    int total_number_of_queries;
+    scanf("%d", &total_number_of_queries);
+    
+    int** total_number_of_pages = (int**)malloc(total_number_of_shelves * sizeof(int*));
+    int* total_number_of_books = (int*)calloc(total_number_of_shelves,sizeof(int));
+
+    while (total_number_of_queries > 0) {
+        addRequest(&query);
+        addQuery(&query_collection,query);
+        total_number_of_queries--;
+    }
+    
+    for(int i=0; i < query_collection.number_of_queries; i++){
+        struct Query current_query = query_collection.queries[i];
+        if (current_query.type_of_query == 1) {
+            int x = current_query.request[0];
+            int y = current_query.request[1];
+            total_number_of_pages[x] = (int*)realloc(total_number_of_pages[x], (total_number_of_books[x]+1) * sizeof(int));
+            total_number_of_pages[x][total_number_of_books[x]] = y;
+            total_number_of_books[x]++;
+        } else if (current_query.type_of_query == 2) {
+            int x = current_query.request[0];
+            int y = current_query.request[1];
+            printf("%d\n", *(*(total_number_of_pages + x) + y));
+        } else {
+            int x = current_query.request[0];
+            printf("%d\n", *(total_number_of_books + x));
+        }
     }
 
-    // Transform the parameters as integers.
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+    if (total_number_of_books) {
+        free(total_number_of_books);
+    }
+    
+    for (int i = 0; i < total_number_of_shelves; i++) {
+        if (*(total_number_of_pages + i)) {
+            free(*(total_number_of_pages + i));
+        }
+    }
+    
+    if (total_number_of_pages) {
+        free(total_number_of_pages);
+    }
 
-    int result = sum(a,b);
-    printf("%d.\n", result);
-
+    if(query.request == NULL){
+        free(query.request);
+    }
+    if(query_collection.queries == NULL){
+        free(query_collection.queries);
+    }
+    
     return 0;
 }
--- C/dinamicArray/src/dinamicArray.c
+++ C/dinamicArray/src/dinamicArray.c
@@ -1,66 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "dinamicArray.h"
 
-/*
- * This stores the total number of books in each shelf.
- */
-int* total_number_of_books;
+void initializeQueryCollection(struct QueryCollection* query_col){
+    query_col->queries = NULL;
+    query_col->number_of_queries = 0;
+}
 
-/*
- * This stores the total number of pages in each book of each shelf.
- * The rows represent the shelves and the columns represent the books.
- */
-int** total_number_of_pages;
+void displayQuery(const struct Query* query){
+    printf("Type of query: %d\n", query->type_of_query);
+    printf("Request:\n");
+    for(int i = 0; i < 2; i++){
+        printf("%d, ", query->request[i]);
+    }
+    printf("\n");
+}
 
-int main()
-{
-    int total_number_of_shelves;
-    scanf("%d", &total_number_of_shelves);
-    
-    int total_number_of_queries;
-    scanf("%d", &total_number_of_queries);
-    
-    int** total_number_of_pages = (int**)malloc(total_number_of_shelves * sizeof(int*));
-    int* total_number_of_books = (int*)calloc(total_number_of_shelves,sizeof(int));
-    
-    while (total_number_of_queries--) {
-        int type_of_query;
-        scanf("%d", &type_of_query);
-        
-        if (type_of_query == 1) {
-            /*
-             * Process the query of first type here.
-             */
-            int x, y;
-            scanf("%d %d", &x, &y);
-            total_number_of_pages[x] = (int*)realloc(total_number_of_pages[x], (total_number_of_books[x]+1) * sizeof(int));
-            total_number_of_pages[x][total_number_of_books[x]] = y;
-            total_number_of_books[x]++;
+void displayQueryCollection(const struct QueryCollection* query_collection){
+    printf("Amount of queries: %d\n", query_collection->number_of_queries);
+    printf("Queries:\n");
+    for(int i = 0; i < query_collection->number_of_queries; i++){
+        displayQuery(&query_collection->queries[i]);
+    }
+    printf("\n");
+}
 
-        } else if (type_of_query == 2) {
-            int x, y;
-            scanf("%d %d", &x, &y);
-            printf("%d\n", *(*(total_number_of_pages + x) + y));
-        } else {
-            int x;
-            scanf("%d", &x);
-            printf("%d\n", *(total_number_of_books + x));
+void addRequest(struct Query* query){
+    scanf("%d",&query->type_of_query);
+    if(query->type_of_query == 1 || query->type_of_query == 2){
+        query->request = (int*)malloc(2 * sizeof(int));
+        if(query->request == NULL){
+            printf("Adding queries failed due to memory allocation.\n");
+            exit(1);
         }
+        scanf("%d",&query->request[0]);
+        scanf("%d",&query->request[1]);
     }
-
-    if (total_number_of_books) {
-        free(total_number_of_books);
-    }
-    
-    for (int i = 0; i < total_number_of_shelves; i++) {
-        if (*(total_number_of_pages + i)) {
-            free(*(total_number_of_pages + i));
+    else if(query->type_of_query == 3){
+        query->request = (int*)malloc(1 * sizeof(int));
+        if(query->request == NULL){
+            printf("Adding queries failed due to memory allocation.\n");
+            exit(1);
         }
+        scanf("%d",&query->request[0]);
     }
-    
-    if (total_number_of_pages) {
-        free(total_number_of_pages);
+    else{
+        printf("Error. Parameter not between 1, 2 or 3.\n");
+        exit(1);
     }
-    
-    return 0;
+}
+
+void addQuery(struct QueryCollection* query_col, const struct Query query){
+    query_col->queries = (struct Query*)realloc(query_col->queries, (query_col->number_of_queries+1) * sizeof(struct Query));
+    //query_col->queries = (struct Query*)malloc((query_col->number_of_queries+1) * sizeof(struct Query));
+    if(query_col->queries == NULL){
+        printf("Adding queries failed due to memory allocation.\n");
+        exit(1);
+    }
+    // Add the new query
+    query_col->queries[query_col->number_of_queries] = query;
+    query_col->number_of_queries++;
 }
