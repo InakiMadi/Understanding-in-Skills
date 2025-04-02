1fbdea31cf20020698d4e4d9ec0e282ebdc643b8
InakiMadi <inakimadi@gmail.com>
2025-04-02 21:20:55 +0200

    Grid exercise, with clean code, reading files, tests

--- Python/Grid/src/GridOrder.py
+++ Python/Grid/src/GridOrder.py
@@ -6,7 +6,7 @@ from typing import List
 class Grid:
     grid: List[str]
 
-    def __init__(self, grid):
+    def __init__(self, grid: List[str]):
         self.grid = grid
 
     def grid_challenge(self) -> str:
@@ -18,13 +18,13 @@ class Grid:
 
         for letter_index in range(word_length):
             column_word = ''.join([word[letter_index] for word in self.grid])
-            if not is_word_ordered(column_word):
+            if not self.is_word_ordered(column_word):
                 return "NO"
         return "YES"
 
     def rearrange_rows_alphabetical_order(self) -> None:
         for index, word in enumerate(self.grid):
-            self.grid[index] = alphabetical_rearrange(word)
+            self.grid[index] = self.alphabetical_rearrange(word)
 
     def validate_every_word_same_length(self, word_length: int) -> None:
         for i in range(1, len(self.grid)):
@@ -42,19 +42,13 @@ class Grid:
             sorted_word += letter
         return sorted_word
 
+    @staticmethod
+    def alphabetical_rearrange(word: str) -> str:
+        # return ''.join(sorted(word))
+        sorted_letters = sorted(word)
+        return Grid.array_letters_to_string(sorted_letters)
 
-def alphabetical_rearrange(word: str) -> str:
-    # return ''.join(sorted(word))
-    sorted_letters = sorted(word)
-    return Grid.array_letters_to_string(sorted_letters)
-
-
-def is_word_ordered(word: str) -> bool:
-    sorted_word = ''.join(sorted(word))
-    return word == sorted_word
-
-
-
-
-
-
+    @staticmethod
+    def is_word_ordered(word: str) -> bool:
+        sorted_word = ''.join(sorted(word))
+        return word == sorted_word
